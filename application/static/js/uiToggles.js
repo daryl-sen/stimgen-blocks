6712a1f27e0b036a28a521d10f96bd8c5e2340db
daryl <tsdaryl@gmail.com>
2021-04-16 17:07:58 -0700

    python app setup

---
+++ application/static/js/uiToggles.js
@@ -0,0 +1,23 @@
+const mobileNavButton = document.getElementById('mobile-nav-button');
+const mobileNavMenu = document.getElementById('nav-links');
+const shades = document.getElementById('shades');
+const closeMenuButton = document.getElementById('close-button');
+
+const toggleElement = (id) => {
+  target = document.getElementById(id);
+  target.classList.toggle('hidden');
+};
+
+const toggleMenu = () => {
+  shades.classList.toggle('hidden');
+  mobileNavMenu.classList.toggle('display-block')
+}
+
+const dismissNotif = (id) => {
+  target = document.getElementById(id);
+  target.classList.toggle('hidden');
+}
+
+mobileNavButton.addEventListener('click', toggleMenu);
+shades.addEventListener('click', toggleMenu);
+closeMenuButton.addEventListener('click', toggleMenu);
